70bb3d64c33d9fbb23573354243f9d6e706b39e5
Viktor Zaremba <c_vzaremba@groupon.com>
2023-06-21 09:51:21 -0500

    Add error handling to handle potential errors during geocoding and when calling the WeatherService

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -5,13 +5,22 @@ class WeatherController < ApplicationController
   def show
     address = params[:address]
     @location = Geocoder.search(address).first
+    @error = "Error geocoding address: #{address}" unless @location
 
-    coordinates = [@location.data['lat'], @location.data['lon']]
-    cache_key = "forecast_#{coordinates.join('_')}"
+    if @location
+      coordinates = [@location.data['lat'], @location.data['lon']]
+      cache_key = "forecast_#{coordinates.join('_')}"
+      @from_cache = Rails.cache.exist?(cache_key)
 
-    @from_cache = Rails.cache.exist?(cache_key)
-    @forecast = Rails.cache.fetch(cache_key, expires_in: 30.minutes) do
-      WeatherService.new(coordinates).forecast
+      @forecast = Rails.cache.fetch(cache_key, expires_in: 30.minutes) do
+        WeatherService.new(coordinates).forecast
+      end
+
+      @error = "Error fetching weather for coordinates: #{coordinates}" unless @forecast
     end
+  rescue => e
+    Rails.logger.error "Error in show action: #{e.message}"
+    @error = "There was an error processing your request. Please try again."
+    render 'show'
   end
 end
