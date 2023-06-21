6e4830922e70f79376f35f0d42c65dddb4725810
Viktor Zaremba <c_vzaremba@groupon.com>
2023-06-20 20:15:17 -0500

    Add weather API call

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -5,6 +5,16 @@ class WeatherController < ApplicationController
   def show
     address = params[:address]
     coordinates = Geocoder.coordinates(address)
+    @forecast = get_forecast(coordinates)
     @location = Geocoder.search(address).first
+  end
+
+  private
+
+  def get_forecast(coordinates)
+    url = 'http://api.openweathermap.org/data/2.5/weather'
+    params = { lat: coordinates[0], lon: coordinates[1], APPID: ENV['OPENWEATHER_API_KEY'], units: 'imperial' }
+    response = Faraday.get(url, params)
+    JSON.parse(response.body)
   end
 end
