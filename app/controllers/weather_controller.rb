b9cfd0f09afeb703a6e77c32bb2b87f55b62033e
Viktor Zaremba <c_vzaremba@groupon.com>
2023-06-20 20:52:20 -0500

    Add caching

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -12,9 +12,13 @@ class WeatherController < ApplicationController
   private
 
   def get_forecast(coordinates)
-    url = 'http://api.openweathermap.org/data/2.5/weather'
-    params = { lat: coordinates[0], lon: coordinates[1], APPID: ENV['OPENWEATHER_API_KEY'], units: 'imperial' }
-    response = Faraday.get(url, params)
-    JSON.parse(response.body)
+    cache_key = "forecast_#{coordinates.join('_')}"
+    @from_cache = Rails.cache.exist?(cache_key)
+    Rails.cache.fetch(cache_key, expires_in: 30.minutes) do
+      url = 'http://api.openweathermap.org/data/2.5/weather'
+      params = { lat: coordinates[0], lon: coordinates[1], APPID: ENV['OPENWEATHER_API_KEY'], units: 'imperial' }
+      response = Faraday.get(url, params)
+      JSON.parse(response.body)
+    end
   end
 end
